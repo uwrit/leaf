3091fe018088b4226e53a9766fb914c7439fea92
Cliff Spital <cspital@uw.edu>
2019-03-29 09:59:56 -0700

    copyright line clarified to include University of Washington and Sean Mooney

--- scripts/leafdb.sh
+++ scripts/leafdb.sh
@@ -10,8 +10,8 @@ BUILD_FILE = 'BUILD_FILE'
 DATA_FILE = 'DATA_FILE'
 PER_FILE_DIR = 'PER_FILE_DIR'
 
-sql_license = '''-- Copyright (c) 2019, UW Medicine Research IT
--- Developed by Nic Dobbins and Cliff Spital
+sql_license = '''-- Copyright (c) 2019, UW Medicine Research IT, University of Washington
+-- Developed by Nic Dobbins and Cliff Spital, CRIO Sean Mooney
 -- This Source Code Form is subject to the terms of the Mozilla Public
 -- License, v. 2.0. If a copy of the MPL was not distributed with this
 -- file, You can obtain one at http://mozilla.org/MPL/2.0/.
--- src/ui-client/src/containers/CohortCountBox/CohortCountBox.css
+++ src/ui-client/src/containers/CohortCountBox/CohortCountBox.css
@@ -1,5 +1,5 @@
-/* Copyright (c) 2019, UW Medicine Research IT
- * Developed by Nic Dobbins and Cliff Spital
+/* Copyright (c) 2019, UW Medicine Research IT, University of Washington
+ * Developed by Nic Dobbins and Cliff Spital, CRIO Sean Mooney
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
