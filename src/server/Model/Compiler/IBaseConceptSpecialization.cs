3f08e5dc52a7717d74f498238f24db14d0d5c4ef
Cliff Spital <cspital@uw.edu>
2019-04-11 14:44:02 -0700

    add license

--- src/server/Model/Compiler/IBaseConceptSpecialization.cs
+++ src/server/Model/Compiler/IBaseConceptSpecialization.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿// Copyright (c) 2019, UW Medicine Research IT, University of Washington
+// Developed by Nic Dobbins and Cliff Spital, CRIO Sean Mooney
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+using System;
 using Model.Tagging;
 
 namespace Model.Compiler
