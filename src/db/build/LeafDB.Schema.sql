3458322cd0f40366510f65c278672a9461028cc0
Nicholas J Dobbins <ndobb@uw.edu>
2019-05-24 11:17:39 -0700

    Added stored procedure for auto-calculating patient counts

--- src/db/build/LeafDB.Schema.sql
+++ src/db/build/LeafDB.Schema.sql
@@ -4080,6 +4080,127 @@ END
 
 
 
+GO
+/****** Object:  StoredProcedure [app].[sp_CalculatePatientCounts]    Script Date: 5/23/19 3:52:36 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+-- =======================================
+-- Author:      Nic Dobbins
+-- Create date: 2019/5/23
+-- Description: Loops through Concepts and auto-calculates patient counts.
+-- =======================================
+CREATE PROCEDURE [app].[sp_CalculatePatientCounts]
+
+@PersonIdField NVARCHAR(50),
+@TargetDataBaseName NVARCHAR(50),
+@TotalAllowedRuntimeInMinutes INT,
+@PerRootConceptAllowedRuntimeInMinutes INT,
+@SpecificRootConcept UNIQUEIDENTIFIER = NULL
+
+AS
+BEGIN
+
+	SELECT Id
+		 , RowNumber = DENSE_RANK() OVER(ORDER BY Id)
+	INTO #Roots
+	FROM app.Concept
+	WHERE IsRoot = 1
+		  AND (Id = @SpecificRootConcept OR @SpecificRootConcept IS NULL)
+
+	DECLARE @TotalRoots INT = (SELECT MAX(RowNumber) FROM #roots),
+			@CurrentRoot INT = 1,
+			@CurrentRootId UNIQUEIDENTIFIER,
+			@TotalConcepts INT,
+			@CurrentConcept INT = 1,
+			@CurrentConceptId UNIQUEIDENTIFIER,
+			@From NVARCHAR(MAX),
+			@Where NVARCHAR(MAX),
+			@Date NVARCHAR(200),
+			@isEncounterBased BIT,
+			@SetMarker NVARCHAR(5) = '@',
+			@SetAlias NVARCHAR(5) = '_T',
+			@TimeLimit DATETIME = DATEADD(MINUTE,@TotalAllowedRuntimeInMinutes,GETDATE()),
+			@TimeLimitPerRootConcept DATETIME = DATEADD(MINUTE,@PerRootConceptAllowedRuntimeInMinutes,GETDATE()),
+			@PerRootConceptRowLimit INT = 50000,
+			@CurrentDateTime DATETIME = GETDATE()
+	
+
+	------------------------------------------------------------------------------------------------------------------------------ 
+	-- ForEach root concept
+	------------------------------------------------------------------------------------------------------------------------------
+	WHILE @CurrentRoot <= @TotalRoots AND @CurrentDateTime < @TimeLimit
+
+	BEGIN
+		
+		SET @CurrentRootId = (SELECT Id FROM #roots WHERE RowNumber = @CurrentRoot)
+
+		BEGIN TRY DROP TABLE #Concepts END TRY BEGIN CATCH END CATCH
+
+		-- Find all children concepts under current root concept
+		SELECT TOP (@PerRootConceptRowLimit)
+			   c.Id
+			 , SqlSetFrom = REPLACE(s.SqlSetFrom,@SetMarker,@SetAlias)
+			 , SqlSetWhere = REPLACE(c.SqlSetWhere,@SetMarker,@SetAlias)
+			 , SqlFieldDate = REPLACE(s.SqlFieldDate,@SetMarker,@SetAlias)
+			 , isEncounterBased
+			 , RowNumber = DENSE_RANK() OVER(ORDER BY PatientCountLastUpdateDateTime,c.Id)
+		INTO #Concepts
+		FROM app.Concept AS c
+			 LEFT JOIN app.ConceptSqlSet AS s
+				ON c.SqlSetId = s.Id
+		WHERE c.RootId = @CurrentRootId
+			  AND c.IsPatientCountAutoCalculated = 1
+		ORDER BY PatientCountLastUpdateDateTime ASC
+
+		SET @TotalConcepts = @@ROWCOUNT
+
+		------------------------------------------------------------------------------------------------------------------------------
+		-- ForEach concept in concepts
+		------------------------------------------------------------------------------------------------------------------------------
+		WHILE @CurrentConcept <= @TotalConcepts AND @CurrentDateTime < @TimeLimit AND @CurrentDateTime < @TimeLimitPerRootConcept
+
+		BEGIN 
+
+			SELECT @From = C.SqlSetFrom
+				 , @Where = C.SqlSetWhere
+				 , @Date = C.SqlFieldDate
+				 , @isEncounterBased = C.isEncounterBased
+				 , @CurrentConceptId = Id
+			FROM #Concepts C
+			WHERE RowNumber = @CurrentConcept
+
+			BEGIN TRY 
+			
+				-- Calculate patient counts for this concept
+				EXECUTE app.sp_CalculateConceptPatientCount
+					@PersonIdField,
+					@TargetDatabaseName,
+					@From,
+					@Where,
+					@Date,
+					@isEncounterBased,
+					@CurrentRootId,
+					@CurrentConceptId
+
+			END TRY BEGIN CATCH END CATCH
+
+			-- Increment the @CurrentConcept parameter
+			SET @CurrentConcept = @CurrentConcept + 1
+			SET @CurrentDateTime = GETDATE()
+
+		END 
+		-- End ForEach concept
+
+		SET @CurrentRoot = @CurrentRoot + 1
+		SET @TimeLimitPerRootConcept = DATEADD(MINUTE,@PerRootConceptAllowedRuntimeInMinutes,GETDATE())
+
+	END 
+	-- End ForEach root concept
+
+
+END
 
 
 
--- src/ui-client/src/components/FindPatients/ConceptTree/LearnMoreButton.tsx
+++ src/ui-client/src/components/FindPatients/ConceptTree/LearnMoreButton.tsx
@@ -8,12 +8,11 @@
 import React from 'react';
 import { FiBarChart2 } from 'react-icons/fi';
 import { Bar, BarChart, LabelList, ResponsiveContainer, XAxis } from 'recharts';
-import { Concept, PatientCountPerYear } from '../../../models/concept/Concept';
+import { Concept, PatientCountPerYear, PatientCountPerYearGrouped, DisplayablePatientCountPerYear } from '../../../models/concept/Concept';
 import { formatLargeNumber, formatSmallNumber } from '../../../utils/formatNumber';
 import PopupBox from '../../Other/PopupBox/PopupBox';
-import './LearnMoreButton.css';
-import { TextArea } from '../../Admin/ConceptEditor/Sections/TextArea';
 import TextareaAutosize from 'react-textarea-autosize';
+import './LearnMoreButton.css';
 
 interface Props {
     concept: Concept;
@@ -24,9 +23,47 @@ interface State {
     showInfoBox: boolean;
 }
 
-const className = 'concept-tree-learn-more'
+/*
+const testYears: PatientCountPerYear[] = [
+    { patientCount: 341 },
+    { year: 1994, patientCount: 622 },
+    { year: 1995, patientCount: 231 },
+    { year: 1996, patientCount: 522 },
+    { year: 1997, patientCount: 324 },
+    { year: 1998, patientCount: 787 },
+    { year: 1999, patientCount: 231 },
+    { year: 2000, patientCount: 622 },
+    { year: 2001, patientCount: 212 },
+    { year: 2002, patientCount: 123 },
+    { year: 2003, patientCount: 772 },
+    { year: 2004, patientCount: 341 },
+    { year: 2005, patientCount: 232 },
+    { year: 2006, patientCount: 231 },
+    { year: 2007, patientCount: 152 },
+    { year: 2008, patientCount: 123 },
+    { year: 2009, patientCount: 789 },
+    { year: 2010, patientCount: 342 },
+    { year: 2011, patientCount: 123 },
+    { year: 2012, patientCount: 231 },
+    { year: 2013, patientCount: 235 },
+    { year: 2014, patientCount: 908 },
+    { year: 2015, patientCount: 458 },
+    { year: 2016, patientCount: 632 },
+    { year: 2017, patientCount: 341 },
+    { year: 2018, patientCount: 342 },
+    { year: 2019, patientCount: 214 },
+    { year: 2020, patientCount: 528 }
+];
+*/
 
 export default class LearnMoreButton extends React.PureComponent<Props,State> {
+    private className = 'concept-tree-learn-more';
+    private startYear = 1995;
+    private currYear = new Date().getFullYear();
+    private height = 250;
+    private margin = {top: 20, right: 20, left: 20, bottom: 20};
+    private minWidth = 300;
+    private maxWidth = 1000;
     constructor(props: Props) {
         super(props);
         this.state = {
@@ -34,67 +71,60 @@ export default class LearnMoreButton extends React.PureComponent<Props,State> {
         }
     }
 
-    public handleClick = (e: any) => { 
-        if (e.target.className === e.currentTarget.className || !this.state.showInfoBox) {
-            const domRect: DOMRect = e.target.getBoundingClientRect();
-            this.setState({ showInfoBox: !this.state.showInfoBox, DOMRect: domRect });
-        }
-    }
-
-    public handleInfoBoxClickedOutside = () => {
-        this.setState({ showInfoBox: !this.state.showInfoBox });
-    }
-
     public render(): any {
         const { concept } = this.props;
+        const c = this.className;
         const countsByYear = concept.uiDisplayPatientCountByYear;
-        const height = 250;
-        const margin = {top: 20, right: 20, left: 20, bottom: 20};
-        const minWidth = 200;
-        const calcWidth = countsByYear ? (countsByYear.length * 40) : 200;
-        const width = calcWidth < minWidth ? minWidth : calcWidth;
+        const calcWidth = countsByYear ? (countsByYear.length * 40) : this.minWidth;
         const formatterThreshold = 10000;
         const data = countsByYear
-            ? countsByYear!
-                .map((p: PatientCountPerYear) => ({ ...p, label: p.patientCount >= formatterThreshold 
+            ? this.groupYears(countsByYear)
+                .map((p: DisplayablePatientCountPerYear) => ({ ...p, label: p.patientCount >= formatterThreshold 
                     ? formatLargeNumber(p.patientCount)
                     : formatSmallNumber(p.patientCount)
                 }))
             : countsByYear;
+        const width = calcWidth < this.minWidth 
+            ? this.minWidth : calcWidth > this.maxWidth 
+            ? this.maxWidth : calcWidth;
 
         return (
-            <span className={`${className}-button`} onClick={this.handleClick}>
+            <span className={`${c}-button`} onClick={this.handleClick}>
                 <FiBarChart2 />
                 <div>
                     Learn More
+
+                    {/* Popup Box */}
                     {this.state.showInfoBox &&
                     <PopupBox 
                         parentDomRect={this.state.DOMRect!} 
                         toggle={this.handleInfoBoxClickedOutside}>
-                        <div className={`${className}`}>
-                            <div className={`${className}-title`}>
+                        <div className={`${c}`} style={{ width }}>
+                            <div className={`${c}-title`}>
                                 <p>{concept.uiDisplayName}</p>
-                                <p className={`${className}-universalid`}>
+                                <p className={`${c}-universalid`}>
                                     {concept.universalId
-                                        ? <span className={`${className}-universalid-value`}>{concept.universalId}</span> 
-                                        : <span className={`${className}-universalid-none`}>local concept only</span> 
+                                        ? <span className={`${c}-universalid-value`}>{concept.universalId}</span> 
+                                        : <span className={`${c}-universalid-none`}>local concept only</span> 
                                     }
                                 </p>
                             </div>
-                            <div className={`${className}-separator`} />
+                            <div className={`${c}-separator`} />
+
+                            {/* Counts by Year chart */}
                             {countsByYear &&
-                            <ResponsiveContainer height={height}>
+                            <ResponsiveContainer height={this.height}>
                             <BarChart 
                                 data={data} 
                                 barCategoryGap={1} 
-                                className={`${className}-chart`}
-                                margin={margin}>
+                                className={`${c}-chart`}
+                                margin={this.margin}>
                                 <XAxis 
                                     dataKey="year" 
                                     interval={0} 
                                     axisLine={false} 
                                     tickLine={false} 
-                                    label={{ value: 'Unique patients by Year', position: 'bottom', className:`${className}-axis-label` }}/>
+                                    label={{ value: 'Unique patients by Year', position: 'bottom', className:`${c}-axis-label` }}/>
                                 <Bar 
                                     barSize={35}
                                     dataKey="patientCount" 
@@ -106,9 +136,12 @@ export default class LearnMoreButton extends React.PureComponent<Props,State> {
                                 </Bar>
                             </BarChart>
                             </ResponsiveContainer>}
-                            {countsByYear && <div className={`${className}-separator-long`} />}
+
+                            {countsByYear && <div className={`${c}-separator-long`} />}
+
+                            {/* Tooltip / Info */}
                             {concept.uiDisplayTooltip &&
-                            <div className={`${className}-info`}>
+                            <div className={`${c}-info`}>
                                 <TextareaAutosize
                                     readOnly={true}
                                     spellCheck={false}
@@ -117,15 +150,46 @@ export default class LearnMoreButton extends React.PureComponent<Props,State> {
                             </div>
                             }
                             {!concept.uiDisplayTooltip &&
-                            <p className={`${className}-noinfo`}>
+                            <p className={`${c}-noinfo`}>
                                 No information provided for this concept
                             </p>
                             }
+
                         </div>    
                     </PopupBox>
                     }
                 </div>
             </span>
         )       
+    }
+
+    private handleClick = (e: any) => { 
+        if (e.target.className === e.currentTarget.className || !this.state.showInfoBox) {
+            const domRect: DOMRect = e.target.getBoundingClientRect();
+            this.setState({ showInfoBox: !this.state.showInfoBox, DOMRect: domRect });
+        }
+    }
+
+    private handleInfoBoxClickedOutside = () => {
+        this.setState({ showInfoBox: !this.state.showInfoBox });
+    }
+
+    private groupYears = (ungrouped: PatientCountPerYear[]): PatientCountPerYearGrouped[] => {
+        const output: PatientCountPerYearGrouped[] = [];
+        const nullYear: PatientCountPerYearGrouped = { year: '?', patientCount: 0 };
+        const startYear: PatientCountPerYearGrouped = { year: `<${this.startYear}`, patientCount: 0 };
+        const greaterThanCurrentYear: PatientCountPerYearGrouped = { year: `>${this.currYear}`, patientCount: 0 };
+
+        for (const bar of ungrouped) {
+            if (!bar.year)                      { nullYear.patientCount += bar.patientCount; }
+            else if (bar.year < this.startYear) { startYear.patientCount += bar.patientCount; }
+            else if (bar.year > this.currYear)  { greaterThanCurrentYear.patientCount += bar.patientCount; }
+            else                                { output.push({ year: `${bar.year}`, patientCount: bar.patientCount }); }
+        }
+
+        if (startYear.patientCount)              { output.splice(0, 0, startYear); }
+        if (nullYear.patientCount)               { output.splice(0, 0, nullYear); }
+        if (greaterThanCurrentYear.patientCount) { output.push(greaterThanCurrentYear); }
+        return output;
     }
 }
