80cbc953245973532a68d3e4655f6581d13813fb
Cliff Spital <cspital@uw.edu>
2019-05-30 13:39:25 -0700

    add admin dset ctrller

---
+++ src/server/API/Controllers/Admin/AdminDatasetController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using Model.Authorization;
+using Microsoft.Extensions.Logging;
+
+namespace API.Controllers.Admin
+{
+    [Authorize(Policy = Role.Admin)]
+    [Produces("application/json")]
+    [Route("api/admin/dataset")]
+    public class AdminDatasetController : Controller
+    {
+        readonly ILogger<AdminDatasetController> log;
+
+        public AdminDatasetController(
+            ILogger<AdminDatasetController> log)
+        {
+            this.log = log;
+        }
+
+
+    }
+}
