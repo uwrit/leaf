1801b24d6565583034284bb59058f81375e913c9
Mehadi Hassan <mehadi@Mehadis-MacBook-Pro.local>
2019-06-17 14:01:59 -0700

    added tests for ConceptEventDeleteResult, ConceptSqlSetDeleteResult, SpecializationGroupDeleteResult

---
+++ src/server/Tests/ConceptEventDeleteResultTests.cs
@@ -0,0 +1,42 @@
+﻿// Copyright (c) 2019, UW Medicine Research IT, University of Washington
+// Developed by Nic Dobbins and Cliff Spital, CRIO Sean Mooney
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+using System;
+using Xunit;
+using Model.Admin.Compiler;
+namespace Tests
+{
+    public class ConceptEventDeleteResultTests
+    {
+        [Fact]
+        public void Ok_Is_True_When_There_Are_No_ConceptSqlSetDependents()
+        {
+            var conceptEventDeleteResult = new ConceptEventDeleteResult
+            {
+                ConceptSqlSetDependents = new ConceptSqlSetDependent[] {}
+            };
+
+            Assert.True(conceptEventDeleteResult.Ok);
+        }
+
+        [Fact]
+        public void Ok_Is_False_When_There_Are_ConceptSqlSetDependents()
+        {
+            var conceptEventDeleteResult = new ConceptEventDeleteResult
+            {
+                ConceptSqlSetDependents = new ConceptSqlSetDependent[]
+                {
+                    new ConceptSqlSetDependent
+                    {
+                        Id = 1,
+                        SqlSetFrom = "SqlSetFrom-test-string"
+                    }
+                }
+            };
+
+            Assert.False(conceptEventDeleteResult.Ok);
+        }
+    }
+}
---
+++ src/server/Tests/ConceptSqlSetDeleteResultTests.cs
@@ -0,0 +1,54 @@
+﻿// Copyright (c) 2019, UW Medicine Research IT, University of Washington
+// Developed by Nic Dobbins and Cliff Spital, CRIO Sean Mooney
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+using System;
+using Xunit;
+using Model.Admin.Compiler;
+namespace Tests
+{
+    public class ConceptSqlSetDeleteResultTests
+    {
+        [Fact]
+        public void Ok_Is_True_When_There_Are_No_Dependents()
+        {
+            var conceptSqlSetDeleteResult = new ConceptSqlSetDeleteResult
+            {
+                ConceptDependents = new ConceptDependent[] {},
+
+                SpecializationGroupDependents = new SpecializationGroupDependent[] {}
+            };
+
+            Assert.True(conceptSqlSetDeleteResult.Ok);
+        }
+
+        [Fact]
+        public void Ok_Is_False_When_There_Are_Dependents()
+        {
+            var conceptSqlSetDeleteResult = new ConceptSqlSetDeleteResult
+            {
+                ConceptDependents = new ConceptDependent[]
+                {
+                    new ConceptDependent
+                    {
+                        Id = Guid.NewGuid(),
+                        UniversalId = "UniversalId-test-string",
+                        UiDisplayName = "UiDisplayName-test-string"
+                    }
+                },
+
+                SpecializationGroupDependents = new SpecializationGroupDependent[]
+                {
+                    new SpecializationGroupDependent
+                    {
+                        Id = 1,
+                        UiDefaultText = "UiDefaultText-test-string"
+                    }
+                }
+            };
+
+            Assert.False(conceptSqlSetDeleteResult.Ok);
+        }
+    }
+}
