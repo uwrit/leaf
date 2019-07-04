bbf357feac192883b9dd1e136db38e4558e7c970
Nicholas J Dobbins <ndobb@uw.edu>
2019-07-04 16:50:20 -0700

    Version bump

--- src/ui-client/src/actions/queries.ts
+++ src/ui-client/src/actions/queries.ts
@@ -141,6 +141,11 @@ export const requestQuerySave = () => {
                     );
                 });
             }));
+
+            /*
+             * Dismiss the modal and query pane. If transitioned simultaneously the pane may 
+             * oddly push content left (pretty sure this is a webkit bug), so put on a delay.
+             */
             dispatch(setNoClickModalState({ message: "Query Saved", state: NotificationStates.Complete }));
             setTimeout(() => dispatch(toggleSaveQueryPane()), 2000);
 
