657dc012587b1c8e7bdd417dd63376017af92813
Nicholas J Dobbins <ndobb@uw.edu>
2019-08-26 15:38:51 -0700

    Sprocs made, working on API

---
+++ src/server/API/Controllers/Admin/AdminPanelFilterController.cs
@@ -0,0 +1,43 @@
+﻿// Copyright (c) 2019, UW Medicine Research IT, University of Washington
+// Developed by Nic Dobbins and Cliff Spital, CRIO Sean Mooney
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+using System;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Linq;
+using API.DTO.Admin;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Model.Network;
+using Model.Authorization;
+using Model.Error;
+using Model.Admin.Network;
+using API.DTO.Admin.Network;
+using API.DTO.Admin.Compiler;
+
+namespace API.Controllers.Admin
+{
+    [Authorize(Policy = Role.Admin)]
+    [Produces("application/json")]
+    [Route("api/admin/panelfilter")]
+    public class AdminPanelFilterController : Controller
+    {
+        readonly ILogger<AdminPanelFilterController> logger;
+
+        public AdminPanelFilterController(
+            ILogger<AdminPanelFilterController> logger)
+        {
+            this.logger = logger;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AdminPanelFilterDTO>>> Get()
+        {
+            return NotFound();
+        }
+    }
+}
---
+++ src/server/API/DTO/Admin/Compiler/AdminPanelFilterDTO.cs
@@ -0,0 +1,55 @@
+﻿// Copyright (c) 2019, UW Medicine Research IT, University of Washington
+// Developed by Nic Dobbins and Cliff Spital, CRIO Sean Mooney
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+using System;
+using System.Collections.Generic;
+using Model.Admin.Compiler;
+
+namespace API.DTO.Admin.Compiler
+{
+    public class AdminPanelFilterDTO
+    {
+        public IEnumerable<PanelFilterDTO> PanelFilters { get; set; }
+        public IEnumerable<GlobalPanelFilterDTO> GlobalPanelFilters { get; set; }
+    }
+
+    public class PanelFilterDTO
+    {
+        public int Id { get; set; }
+        public Guid ConceptId { get; set; }
+        public string UiDisplayText { get; set; }
+        public string UiDisplayDescription { get; set; }
+
+        public PanelFilterDTO() { }
+
+        public PanelFilterDTO(PanelFilter panelFilter)
+        {
+            Id = panelFilter.Id;
+            ConceptId = panelFilter.ConceptId;
+            UiDisplayText = panelFilter.UiDisplayText;
+            UiDisplayDescription = panelFilter.UiDisplayDescription;
+        }
+    }
+
+    public class GlobalPanelFilterDTO
+    {
+        public int Id { get; set; }
+        public AccessMode AccessMode { get; set; }
+        public bool IsInclusion { get; set; }
+        public int SqlSetId { get; set; }
+        public string SqlSetWhere { get; set; }
+
+        public GlobalPanelFilterDTO() { }
+
+        public GlobalPanelFilterDTO(GlobalPanelFilter filter)
+        {
+            Id = filter.Id;
+            AccessMode = filter.AccessMode;
+            IsInclusion = filter.IsInclusion;
+            SqlSetId = filter.SqlSetId;
+            SqlSetWhere = filter.SqlSetWhere;
+        }
+    }
+}
