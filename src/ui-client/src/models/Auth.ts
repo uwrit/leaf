d8eb1f438eb40ad77d9187a2e4a3868132fd1e9d
Nic Dobbins <ndobb@uw.edu>
2019-08-30 15:03:43 -0700

    Jwt roles fix (#273)
    * Added check for jwt roles property, bumped version
    
    * Bumpbed version on db init sql file

--- src/ui-client/src/models/Auth.ts
+++ src/ui-client/src/models/Auth.ts
@@ -59,7 +59,7 @@ export interface DecodedIdToken {
     'aud': string;
     'auth-type': string;
     'exp': number;
-    'http://schemas.microsoft.com/ws/2008/06/identity/claims/role': string[];
+    'http://schemas.microsoft.com/ws/2008/06/identity/claims/role'?: string[];
     'http://schemas.xmlsoap.org/ws/2005/05/identity/claims/name': string;
     'iat': number;
     'id-nonce': string;
