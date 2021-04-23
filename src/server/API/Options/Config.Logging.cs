920752b864fa6399e1ee4c777959e7cf2e53a9d1
Nic Dobbins <ndobb@uw.edu>
2021-04-23 13:57:35 -0700

    Added SERILOG no delete rule

---
+++ src/server/API/Options/Config.Logging.cs
@@ -0,0 +1,19 @@
+﻿// Copyright (c) 2020, UW Medicine Research IT, University of Washington
+// Developed by Nic Dobbins and Cliff Spital, CRIO Sean Mooney
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+using System;
+
+namespace API.Options
+{
+    public static partial class Config
+    {
+        public static class Logging
+        {
+            public const string Section = @"Logging";
+            public const string Directory = @"SERILOG_DIR";
+            public const string FileTemplate = @"leaf-api-{Date}.log";
+        }
+    }
+}
