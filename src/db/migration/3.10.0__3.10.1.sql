0352586a3a9ca0da6f0683f617f39184cf3c35a6
Nic Dobbins <ndobb@uw.edu>
2021-11-05 16:24:08 -0700

    Updated SQL migration script

--- src/db/migration/3.10.0__3.10.1.sql
+++ src/db/migration/3.10.0__3.10.1.sql
@@ -43,8 +43,8 @@ IF OBJECT_ID('app.Notification') IS NOT NULL
 GO
 CREATE TABLE [app].[Notification](
 	[Id] [uniqueidentifier] NOT NULL,
-    [NotificationMessage] NVARCHAR(2000) NULL,
-    [NotificationUntil] DATETIME,
+    [Message] NVARCHAR(2000) NULL,
+    [Until] DATETIME,
     [Created] DATETIME NOT NULL,
     [CreatedBy] NVARCHAR(1000) NOT NULL,
     [Updated] DATETIME NOT NULL,
@@ -73,12 +73,41 @@ GO
 -- Create date: 2021/11/2
 -- Description: Gets app state
 -- =======================================
-CREATE PROCEDURE [app].[sp_GetAppState]
+CREATE PROCEDURE [app].[sp_GetAppStateAndNotifications]
 AS
 BEGIN
     SET NOCOUNT ON
 
+    -- App state
     SELECT IsUp, DowntimeMessage, DowntimeUntil
+    FROM app.AppState
+
+    -- Notifications
+    SELECT Id, Message
+    FROM app.Notification
+    WHERE Until < GETDATE() OR Until IS NULL
+
+END
+GO
+
+/*
+ * [adm].[sp_GetAppState]
+ */
+IF OBJECT_ID('adm.sp_GetAppState', 'P') IS NOT NULL
+    DROP PROCEDURE [adm].[sp_GetAppState];
+GO
+
+-- =======================================
+-- Author:      Nic Dobbins
+-- Create date: 2021/11/5
+-- Description: Gets app state
+-- =======================================
+CREATE PROCEDURE [adm].[sp_GetAppState]
+AS
+BEGIN
+    SET NOCOUNT ON
+
+    SELECT IsUp, DowntimeMessage, DowntimeUntil, Updated, UpdatedBy
     FROM app.AppState
 
 END
@@ -111,6 +140,86 @@ BEGIN
       , DowntimeUntil = @downtimeUntil
       , Updated = GETDATE()
       , UpdatedBy = @user
+
+END
+GO
+
+/*
+ * [adm].[sp_UpsertNotification]
+ */
+IF OBJECT_ID('adm.sp_UpsertNotification', 'P') IS NOT NULL
+    DROP PROCEDURE [adm].[sp_UpsertNotification];
+GO
+
+-- =======================================
+-- Author:      Nic Dobbins
+-- Create date: 2021/11/5
+-- Description: Updates or inserts a notification
+-- =======================================
+CREATE PROCEDURE [adm].[sp_UpsertNotification]
+    @user NVARCHAR(100),
+    @id UNIQUEIDENTIFIER = NULL,
+    @message NVARCHAR(2000),
+    @until DATETIME
+AS
+BEGIN
+    SET NOCOUNT ON
+
+    DECLARE @upserted TABLE ([Id] [uniqueidentifier], [Message] NVARCHAR(2000), [Until] DATETIME, [Created] DATETIME , [CreatedBy] NVARCHAR(1000), [Updated] DATETIME , [UpdatedBy] NVARCHAR(1000))
+
+    IF @id IS NULL
+    BEGIN
+        INSERT INTO app.Notification ([Message], Until, Created, CreatedBy, Updated, UpdatedBy)
+        OUTPUT inserted.Id, inserted.Message, inserted.Until, inserted.Created, inserted.CreatedBy, inserted.Updated, inserted.UpdatedBy INTO @upserted
+        SELECT @message, @until, GETDATE(), @user, GETDATE(), @user
+    END
+
+    ELSE
+    BEGIN
+        UPDATE app.Notification (NotificationMessage, NotificationUntil, Updated, UpdatedBy)
+        SET [Message] = @message
+          , Until = @until
+          , Updated = GETDATE()
+          , UpdatedBy = @user
+        OUTPUT inserted.Id, inserted.Message, inserted.Until, inserted.Created, inserted.CreatedBy, inserted.Updated, inserted.UpdatedBy INTO @upserted
+        WHERE Id = @id
+    END
+
+    SELECT * FROM @upserted
+
+END
+GO
+
+/*
+ * [adm].[sp_DeleteNotification]
+ */
+IF OBJECT_ID('adm.sp_DeleteNotification', 'P') IS NOT NULL
+    DROP PROCEDURE [adm].[sp_DeleteNotification];
+GO
+
+-- =======================================
+-- Author:      Nic Dobbins
+-- Create date: 2021/11/5
+-- Description: Deletes a notification
+-- =======================================
+CREATE PROCEDURE [adm].[sp_DeleteNotification]
+    @id UNIQUEIDENTIFIER = NULL
+AS
+BEGIN
+    SET NOCOUNT ON
+
+    DECLARE @deleted TABLE ([Id] [uniqueidentifier], [Message] NVARCHAR(2000), [Until] DATETIME, [Created] DATETIME , [CreatedBy] NVARCHAR(1000), [Updated] DATETIME , [UpdatedBy] NVARCHAR(1000))
+
+    IF NOT EXISTS(SELECT 1 FROM app.Notification WHERE Id = @id)
+        BEGIN;
+            THROW 70404, N'Notification not found.', 1;
+        END;
+
+    DELETE FROM app.Notification
+    OUTPUT deleted.Id, deleted.Message, deleted.Until, deleted.Created, deleted.CreatedBy, deleted.Updated, deleted.UpdatedBy INTO @deleted
+    WHERE Id = @id
+
+    SELECT * FROM @deleted
 
 END
 GO
