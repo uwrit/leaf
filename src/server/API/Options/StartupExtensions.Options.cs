d977ee9bf9d4145dfd37cc30b2d5b9f5b7509a9c
Nic Dobbins <ndobb@uw.edu>
2022-04-01 12:18:55 -0700

    fixed hard-coded bit

--- src/server/API/Options/StartupExtensions.Options.cs
+++ src/server/API/Options/StartupExtensions.Options.cs
@@ -407,10 +407,7 @@ namespace API.Options
             // Clin Db Connection
             services.Configure<ClinDbOptions>(opts =>
             {
-                // opts.ConnectionString = config.GetByProxy(Config.Db.Clin.Connection);
-                // opts.ConnectionString = "Server='localhost';Port=3306;Database='mimic4';Uid='root';Pwd='Jefferson407!';";
-
-                opts.ConnectionString = "leafsynpuf";
+                opts.ConnectionString = config.GetByProxy(Config.Db.Clin.Connection);
 
                 opts.DefaultTimeout = config.GetValue<int>(Config.Db.Clin.DefaultTimeout);
                 opts.WithRdbms(config.GetValue<string>(Config.Db.Clin.RDBMS));
